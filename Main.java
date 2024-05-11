64737e31e7bce8959bbf942730296aacba4a4f1d
Alejandro Jaramillo <ajaram1214@gmail.com>
2024-05-10 23:27:23 -0500

    Método agregado para imprimir mi nombre

--- Main.java
+++ Main.java
@@ -1,2 +1,10 @@
 public class Main {
+
+    public static void printAssignment() {
+        System.out.println("Assignment 3 by Alejandro Jaramillo Rodriguez");
+    }
+
+    public static void main(String[] args) {
+        printAssignment();
+    }
 }
